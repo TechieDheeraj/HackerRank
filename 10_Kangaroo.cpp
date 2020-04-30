b7d263590df8e9d2aae109a53be990c924ce6b62
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-04-30 18:22:44 +0530

    Solved Kangaroo Problem

---
+++ 10_Kangaroo.cpp
@@ -0,0 +1,20 @@
+// solved by Techie Dheeraj
+// https://www.hackerrank.com/challenges/kangaroo/problem
+
+string kangaroo(int x1, int v1, int x2, int v2) {
+
+    if ((x2 > x1) && (v2 > v1))
+        return "NO";
+
+    else if ((x2 > x1) && (v1 > v2)) {
+        while (x2 > x1) {
+            x1 += v1;
+            x2 += v2;
+
+            if (x2 == x1)
+                return "YES";
+        }
+    }
+
+    return "NO";
+}
