f5a24634e661cc352c06fe182cc8608b51867a7d
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-05-03 02:24:55 +0530

    All test cases executed successfully

--- 28_SumVsXor.cpp
+++ 28_SumVsXor.cpp
@@ -1,7 +1,30 @@
 // solved by Techie Dheeraj
 // https://www.hackerrank.com/challenges/sum-vs-xor/problem
 
-// TODO: 8/12 test cases passed
+// 12/12 test cases passed
+
+long sumXor(long n) {
+
+    long result = 0;
+    long countZ = 0;
+
+/*
+    OR and XOR result on wherever bit 1 is set is not going to be same because OR will take carry. valid result will be only where bit 0 is available , So counting number of 0 and to calculate possible numbers on bases of number of zeroes will be 2^(no. of zeroes).
+*/
+
+    while (n) {
+        //countZ += (n % 2) ? 0 : 1; // Even value
+        countZ += (n & 1) ? 0 : 1; // Even value
+        n >>= 1;
+    }
+    result = pow(2, countZ);
+
+    return result;
+}
+
+/*
+
+// 8/12 test cases passed
 
 long sumXor(long n) {
 
@@ -14,3 +37,5 @@ long sumXor(long n) {
 
 	return result;
 }
+
+*/
