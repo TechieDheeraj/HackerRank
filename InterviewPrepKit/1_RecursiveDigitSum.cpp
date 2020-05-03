a1b4dd6c90d32400015395d5a300ee3fe7710d9e
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-05-03 17:18:06 +0530

    fixed Recursive Digit Sum problem

--- InterviewPrepKit/1_RecursiveDigitSum.cpp
+++ InterviewPrepKit/1_RecursiveDigitSum.cpp
@@ -1,43 +1,36 @@
 // Solved by Techie Dheeraj
 // https://www.hackerrank.com/challenges/recursive-digit-sum/problem
 
-// TODO: 9/12 Test Cases Passed
+string find_superDigit(string s){
 
-int find_superDigit(vector<int>arr){
+    long long int sum =0;
+    string str;
 
-    long int sum = 0;
-    vector<int>sumV;
-
-    if ( arr.size() == 1 && arr[0] <= 9)
-        return arr[0];
-
-    for (auto &i : arr)
-        sum+= i;
-
-    while(sum > 10) {
-        sumV.insert(sumV.begin(), sum % 10);
-        sum = sum / 10;
+    if ( s.size() == 1) {
+        return s;
     }
-    sumV.insert(sumV.begin(), sum % 10);
 
-    return find_superDigit(sumV);
+    for (auto &i : s) {
+        sum += (int)(i - '0');
+    }
+    str = to_string(sum);
+    
+    return find_superDigit(str);
 }
-
+// Complete the superDigit function below.
 int superDigit(string n, int k) {
 
     string p;
-    int digit;
+    string digit;
+    long int sum = 0;
 
-    int size = n.size() * k;
-    vector<int> arr(size);
+    for (int i = 0; i < n.size(); ++i) {
+        sum += (int)(n[i]-'0');
+    }
 
-    for (int i = 0; i < k; ++i)
-        p +=n;
+    sum *= k;
+    p = to_string(sum);
+    digit = find_superDigit(p);
 
-    for ( unsigned int i = 0; i < p.size(); ++i)
-        arr.push_back((int)(p[i]-'0'));
-  
-    digit = find_superDigit(arr);
-
-    return  digit;
+    return digit[0] - '0';
 }
